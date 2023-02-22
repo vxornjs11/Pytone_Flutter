199b827193b7774dd9c2f2ef11d62493351c0fe0
= <vxornjs11@naver.com>
2023-02-22 18:24:52 +0900

    Add kakao link api

--- Flutter-Python/FlutterSpace/local_now_app/lib/main.dart
+++ Flutter-Python/FlutterSpace/local_now_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'home.dart';
 
 void main() {
+  KakaoSdk.init(
+    nativeAppKey: '',
+  );
   runApp(const MyApp());
 }
 
