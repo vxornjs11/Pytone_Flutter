838fb273e319f2ac134c387b7dba7942b1b0bf80
sangwonKim7 <swkim7275@gmail.com>
2023-02-23 18:42:53 +0900

    Update DataAnalysis file / seoul.dart

--- Flutter-Python/FlutterSpace/local_now_app/lib/Seoul/seoul.dart
+++ Flutter-Python/FlutterSpace/local_now_app/lib/Seoul/seoul.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:local_now_app/seoul/seoul_chart.dart';
+import 'package:lottie/lottie.dart';
 
 import '../models/message_seoul.dart';
 import '../widgets/custom_loader.dart';
@@ -22,6 +24,8 @@ class _SeoulState extends State<Seoul> {
   late String gungu;
   late Map<String, dynamic> result;
   late bool onLoad = false;
+  late int selectedItem; // 피커뷰 번호
+  late List imageName;
 
   @override
   void initState() {
@@ -29,6 +33,61 @@ class _SeoulState extends State<Seoul> {
     super.initState();
     gunguController = TextEditingController();
     result = {};
+    selectedItem = 0; // 초기값
+    imageName = [
+      '종로구',
+      '중구',
+      '용산구',
+      '성동구',
+      '광진구',
+      '동대문구',
+      '중랑구',
+      '성북구',
+      '강북구',
+      '도봉구',
+      '노원구',
+      '은평구',
+      '서대문구',
+      '마포구',
+      '양천구',
+      '강서구',
+      '구로구',
+      '금천구',
+      '영등포구',
+      '동작구',
+      '관악구',
+      '서초구',
+      '강남구',
+      '송파구',
+      '강동구'
+    ];
+    // imageName = [
+    //   "Jongno_gu",
+    //   "Jung_gu",
+    //   "Yongsan_gu",
+    //   "Seongdong_gu",
+    //   "Gwangjin_gu",
+    //   "Dongdaemun_gu",
+    //   "Jungnang_gu",
+    //   "Seongbuk_gu",
+    //   "Gangbuk_gu",
+    //   "Dobong_gu",
+    //   'Nowon_gu',
+    //   'Eunpyeong_gu',
+    //   'Seodaemun_gu',
+    //   'Mapo_gu',
+    //   'Yangcheon_gu',
+    //   'Gangseo_gu',
+    //   'Guro_gu',
+    //   'Geumcheon_gu',
+    //   "Yeongdeungpo_gu",
+    //   "Dongjak_gu",
+    //   "Gwanak_gu",
+    //   "Seocho_gu",
+    //   "Gangnam_gu",
+    //   "Songpa_gu",
+    //   "Gangdong_gu"
+    // ];
   }
 
   @override
@@ -49,18 +108,54 @@ class _SeoulState extends State<Seoul> {
                 children: [
                   // 피커뷰로 바꿔주기
                   // 왼쪽에는 구별 이미지 바뀌는거 보여주기
-                  TextField(
-                      controller: gunguController,
-                      decoration: const InputDecoration(labelText: '군구'),
-                      keyboardType: TextInputType.text // 소수점찍기<<
-                      // keyboardType: const TextInputType.numberWithOptions(decimal: true) // 소수점찍기<<
-                      ),
-                  const SizedBox(
-                    height: 30,
+                  // TextField(
+                  //     controller: gunguController,
+                  //     decoration: const InputDecoration(labelText: '군구'),
+                  //     keyboardType: TextInputType.text // 소수점찍기<<
+                  //     // keyboardType: const TextInputType.numberWithOptions(decimal: true) // 소수점찍기<<
+                  //     ),
+
+                  // CircleAvatar(
+                  //   backgroundImage: AssetImage('images/$result.jpg'),
+                  //   radius: 100,
+                  // ),
+                  //// ---------
+                  Lottie.network("https://assets1.lottiefiles.com/packages/lf20_aEtcq1Z1up.json"),
+                  // Image.asset(
+                  //   // 'images/${imageName[selectedItem]}',
+                  //   'images/dream_TradingCard.jpg',
+                  //   // width: 150,
+                  //   height: 300,
+                  // ),
+                  Text('Selected Item: ${imageName[selectedItem]}'),
+                  SizedBox(
+                    // width: 200,
+                    height: 200,
+                    child: CupertinoPicker(
+                      // backgroundColor: Theme.of(context).primaryColorLight,
+                      itemExtent: 30,
+                      onSelectedItemChanged: (value) {
+                        setState(() {
+                          selectedItem = value;
+                        });
+                      },
+                      children: [
+                        for (int i = 0; i < imageName.length; i++) ...[
+                          Text(imageName[i])
+                        ]
+                      ],
+                    ),
                   ),
+                  
                   ElevatedButton(
+                    style: ButtonStyle(
+                        minimumSize: MaterialStatePropertyAll(
+                            Size(MediaQuery.of(context).size.width, 40))),
                     onPressed: () async {
-                      gungu = gunguController.text;
+                      //
+                      gungu = imageName[selectedItem];
+                      // gungu = gunguController.text;
+                      //
                       await getJSONData();
                       MessageSeoul.gungu = gungu;
                       MessageSeoul.resultMap = result;
@@ -76,21 +171,8 @@ class _SeoulState extends State<Seoul> {
                         _showDialog(context, result);
                       });
                     },
-                    child: const Text('입력'),
+                    child: const Text('OK'),
                   ),
-                  // CircleAvatar(
-                  //   backgroundImage: AssetImage('images/$result.jpg'),
-                  //   radius: 100,
-                  // ),
-                  //// ---------
-                  CupertinoPicker(
-                      itemExtent: 30,
-                      onSelectedItemChanged: (value) {
-                        //
-                      },
-                      children: [
-                        //
-                      ])
 
                   //// ---------
                 ],
