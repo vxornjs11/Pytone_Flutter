fa10bfc2219d287d6b0e16564d1414e8c293ba33
LeeHosik <tj0bigdata0l@gmail.com>
2023-02-23 11:35:46 +0900

    충돌확인

---
+++ Flutter-Python/FlutterSpace/local_now_app/lib/hosikTest/onclick.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+@override
+Widget clickedLocal(BuildContext context) {
+  return Scaffold(
+    body: Column(
+      children: [
+        Text('data1'),
+      ],
+    ),
+  );
+}
