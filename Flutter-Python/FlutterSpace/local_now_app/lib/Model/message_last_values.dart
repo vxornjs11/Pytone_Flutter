6790e262ea7efdd412a0f0043d10fd32b1f8f09c
altese <readme42u@gmail.com>
2023-02-24 10:13:21 +0900

    firebase에서 불러오기

---
+++ Flutter-Python/FlutterSpace/local_now_app/lib/Model/message_last_values.dart
@@ -0,0 +1,20 @@
+class MessageLastValues {
+  final String sido;
+  final int babies;
+  final int companies;
+  final int doctors;
+  final int outPop;
+  final int students;
+
+  // static String id = "";
+  // static int babies =
+
+  MessageLastValues({
+    required this.sido,
+    required this.babies,
+    required this.companies,
+    required this.doctors,
+    required this.outPop,
+    required this.students,
+  });
+}
